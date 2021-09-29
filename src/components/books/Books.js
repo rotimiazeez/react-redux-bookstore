d3df2a9b6e17bb05e3a50953c7a4875262960461
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-09-29 20:51:49 +0100

    feat: Implement the add and remove function

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,57 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import AddNewBook from './addNewBook/AddNewBook';
 import Book from './book/Book';
 
 const Books = () => {
-  const myBooks = [
-    {
-      id: 1,
-      name: 'Where the Crawdads Sing',
-      author: 'Delia Owens',
-      category: 'Mystery, Fiction',
-      progress: '50',
-      chapter: 'Chapter 5',
-    },
-    {
-      id: 2,
-      name: 'The Seven Husbands of Evelyn Hugo',
-      author: 'Taylor Jenkins Reid',
-      category: 'Romantic Novel',
-      progress: '80',
-      chapter: 'Chapter 8',
-    },
-    {
-      id: 3,
-      name: 'It Ends with Us',
-      author: 'Colleen Hoover',
-      category: ' Contemporary romance',
-      progress: '59',
-      chapter: 'Chapter 8',
-    },
-    {
-      id: 4,
-      name: 'To Kill a Mockingbird',
-      author: 'Susan Stoker',
-      category: 'Thriller',
-      progress: '15',
-      chapter: 'Chapter 1',
-    },
-    {
-      id: 5,
-      name: 'The Song of Achilles',
-      author: 'Madeline Miller',
-      category: 'Historical Fiction',
-      progress: '30',
-      chapter: 'Chapter 1',
-    },
-  ];
-
+  const { booksReducer } = useSelector((state) => state);
+  const { books } = booksReducer;
   return (
     <div className="books-page">
       <ul className="book-list">
-        {myBooks.map((book) => (
+        {books.map((book) => (
           <li key={book.id} className="book-card">
-            <Book bookInfo={book} />
+            <Book bookInfo={book} progress={Math.floor(Math.random() * 101)} />
           </li>
         ))}
       </ul>
@@ -59,5 +19,4 @@ const Books = () => {
     </div>
   );
 };
-
 export default Books;
--- src/components/books/addNewBook/AddNewBook.js
+++ src/components/books/addNewBook/AddNewBook.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { createBook } from '../../../redux/books/books';
 
-const AddNewBook = () => (
-  <section className="add-new-book-section">
-    <h2>ADD NEW BOOK</h2>
-    <form>
-      <input type="text" name="name" placeholder="Book title" />
-      <select name="categories">
-        <option value="action">Action</option>
-        <option value="drama">Drama</option>
-        <option value="comedy">Comedy</option>
-        <option value="adventure">Adventure</option>
-      </select>
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </section>
-);
+const AddNewBook = () => {
+  const dispatch = useDispatch();
+  const [bookInfo, setBookInfo] = useState({
+    item_id: '',
+    title: '',
+    category: '',
+  });
+  const handleChange = (name) => (e) => {
+    setBookInfo({ ...bookInfo, [name]: e.target.value });
+  };
+  const onClick = () => {
+    setBookInfo({ ...bookInfo, item_id: uuidv4() });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createBook(bookInfo));
+    setBookInfo({ ...bookInfo, title: '', category: '' });
+  };
+
+  return (
+    <section className="add-new-book-section">
+      <h2>ADD NEW BOOK</h2>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <input onChange={handleChange('title')} type="text" name="title" placeholder="Book title" value={bookInfo.title} />
+        <input onChange={handleChange('category')} type="text" name="category" placeholder="Author name" value={bookInfo.category} />
+        <button onClick={onClick} type="submit">ADD BOOK</button>
+      </form>
+    </section>
+  );
+};
 
 export default AddNewBook;
